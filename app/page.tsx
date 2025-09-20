e2e3aaad94028b872e50a07e894bb4386a40294c
Divnoor <divnoorsingh4602@gmail.com>
2025-09-20 13:07:03 -0700

    Excalidraw setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import ExcalidrawCanvas from "@/components/ExcalidrawCanvas";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <ExcalidrawCanvas />;
 };
 
 export default Home;
---
+++ components/ExcalidrawCanvas.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import "@excalidraw/excalidraw/index.css";
+
+const Excalidraw = dynamic(
+  async () => (await import("@excalidraw/excalidraw")).Excalidraw,
+  {
+    ssr: false,
+  }
+);
+export default function ExcalidrawCanvas() {
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <Excalidraw />
+    </div>
+  );
+}
